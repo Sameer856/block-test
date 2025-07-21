e98c8445eb6f26b20271fa3855f0f873af65d254
Muhammed Sameer <sameersmacbookair@Muhammeds-MacBook-Air.local>
2025-07-21 22:29:25 +0530

    Update blink example to use pin 2 with 100ms delay

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,16 +1,10 @@
-
-
-
 void setup() {
-
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
-  
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(1000);
-
+  digitalWrite(2, HIGH);
+  delay(100);
+  digitalWrite(2, LOW);
+  delay(100);
 }
